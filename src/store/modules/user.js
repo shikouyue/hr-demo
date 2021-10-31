55dd8c5ab87d028b044f6a700e88cb0745895e3d
十口月 <1343108345@qq.com>
2021-10-31 15:01:11 +0800

    登录模块逻辑封装完毕

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,33 @@
+import { login } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
+// 状态
+const state = {
+  token: getToken() // 设置token初始状态   token持久化 => 放到缓存中
+}
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+// 执行异步
+const actions = {
+  // 定义login action  也需要参数 调用action时 传递过来的参数
+  async login(context, data) {
+    const res = await login(data)
+    console.log(res)
+    context.commit('setToken', res)
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -8,7 +9,24 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use()
 
-// 相应拦截器
-service.interceptors.response.use()
+// 响应拦截器
+service.interceptors.response.use(
+  (response) => {
+    // axios默认加了一层data
+    const { success, message, data } = response.data
+    //   要根据success的成功与否决定下面的操作
+    if (success) {
+      return data
+    } else {
+      // 业务已经错误了 还能进then ? 不能 ！ 应该进catch
+      Message.error(message) // 提示错误消息
+      return Promise.reject(new Error(message))
+    }
+  },
+  (error) => {
+    Message.error(error.message) // 提示错误信息
+    return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+  }
+)
 
 export default service
